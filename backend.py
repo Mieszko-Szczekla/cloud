d03dd5a57341cd75de575e900c34736a8159b6c6
Mieszko Szczekla <mieszkoszczekla@outlook.com>
2025-05-20 20:25:35 +0200

    Added some fixes

--- backend.py
+++ backend.py
@@ -7,50 +7,37 @@ import os
 rekordy = mysql.connector.connect(
     host=os.getenv("MYSQL_HOST", "localhost").split(":")[0],
     port=5432,
-    user='postgres',
-    password='password',
+    user='backend',
+    password=os.getenv("MYSQL_PASSWORD", "password"),
     database='records'
 )
 
-bucket_name = "avn29rbyczr9b3vz9wbycri3oivbsieuybc3cwi"
+rekordy.get_cursor().execute("CREATE TABLE IF NOT EXISTS rekordy (id INT AUTO_INCREMENT PRIMARY KEY, record VARCHAR(255))")
+
+bucket_name = os.getenv("BUCKET_NAME", "rekordy-bucket")
 s3_client = boto3.client("s3",
     aws_access_key_id=os.getenv("AK_ID", ""), 
     aws_secret_access_key=os.getenv("AK_SECRET", ""), 
     region_name=os.getenv("REGION", "us-east-1"),
-    session_token=os.getenv("SESSION_TOKEN", "")
+    aws_session_token=os.getenv("SESSION_TOKEN", "")
 )
 
 app = Flask(__name__)
 
 
 @app.route("/upload_url", methods=["GET"])
-def upload_url():
-    upload_details = s3_client.generate_presigned_post(
-        bucket_name,
-        request.args.get("filename")
-    )
-    url_trail = "?"
-    for k in upload_details["fields"]:
-        url_trail += f"{k}={upload_details['fields'][k]}&"
-    url_trail = url_trail[:-1]  # Remove the last '&'
-    return upload_details
-
-@app.route("/upload_message", methods=["POST"])
-def upload_message():
+def generate_upload():
     filename = request.args.get("filename")
-    message = request.data.decode("utf-8")
-    res = requests.get("127.0.0.1:8080/upload_url?filename="+filename).json()
-    url = res["url"]
-    with open("temp", 'wb') as file_to_upload:
-        file_to_upload.write(message.encode("utf-8"))
-    with open("temp", 'rb') as file_to_upload:
-        files = {'file': ("temp", file_to_upload)}
-        upload_response = requests.post(
-            res['url'], 
-            data=res['fields'], 
-            files=files
+    url = s3_client.generate_presigned_url(
+            "put_object",
+            Params={
+                "Bucket": bucket_name, 
+                "Key": filename},
+            ExpiresIn=3000
         )
-    
+    response = jsonify({"url": url})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 @app.route("/download_url", methods=["GET"])
 def download_url():
@@ -60,9 +47,11 @@ def download_url():
             "Bucket": bucket_name,
             "Key": request.args.get("filename")
         },
-        ExpiresIn=3600
+        ExpiresIn=3000
     )
-    return jsonify({"url": download_details})
+    response = jsonify({"url": download_details})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 @app.route("/add_record", methods=["GET"])
 def add_record():
@@ -81,7 +70,6 @@ def get_records():
         cur.execute("SELECT * FROM rekordy")
         rekordy_records = cur.fetchall()
         return jsonify(rekordy_records)
-
 
 
 if __name__ == "__main__":
